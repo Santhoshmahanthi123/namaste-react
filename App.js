20e3d5f870408b633eda2ebc3e4bd3c85bbfe821
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2024-07-09 01:55:15 +0530

    updated react app

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 const parent = React.createElement("div",{id: "parent"},[
     React.createElement("div", {id:"child"}, [
-        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h1",{},"I am Santhosh"),
         React.createElement("h2",{},"I am h2 tag"),
     ]),
     React.createElement("div", {id:"child2"}, [
