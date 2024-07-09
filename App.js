53b7e713f6b8bf3e3ddf833c06428064dad95c43
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2024-07-09 16:26:46 +0530

    updated app to jsx

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-const parent = React.createElement("div",{id: "parent"},[
-    React.createElement("div", {id:"child"}, [
-        React.createElement("h1",{},"I am Santhosh"),
-        React.createElement("h2",{},"I am h2 tag"),
-    ]),
-    React.createElement("div", {id:"child2"}, [
-        React.createElement("h1",{},"I am h1 tag child2"),
-        React.createElement("h2",{},"I am h2 tag child2"),
-    ]),
-] )
-const root =  ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
+const jsxHeading = <h1 className="head">Namaste React...</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
