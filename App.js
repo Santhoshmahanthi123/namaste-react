38a364ea92200d922038553c350bca61ff41a6fa
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2024-07-09 16:52:04 +0530

    Adding one component to other

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 const jsxHeading = <h1 className="head">Namaste React...</h1>;
 
 // functional component
+const Title = () => <h1 className="head"> Learning react is fun!!!</h1>;
 
-const HeadingComponent = () => {
-  return <h1 className="head">React Functional Component!</h1>;
+const HeadingComponent2 = () => {
+  return <h1 className="body">React Functional Component!</h1>;
 };
 
-const HeadingComponent2 = () => (
-  <h1 className="head">React Functional Component!</h1>
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="head">React Functional Component!</h1>
+  </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(HeadingComponent());
+root.render(<HeadingComponent />);
