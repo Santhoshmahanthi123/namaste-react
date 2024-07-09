563af7d6d542e2c901d66ce9407d0961d69d16dd
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2024-07-09 19:42:57 +0530

    passing props

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const Header = () => {
 const styleCard = {
   backgroundColor: "skyblue",
 };
-const Restaurantcard = () => {
+const Restaurantcard = ({ resName, cuisine }) => {
   return (
     <div className="res-card" style={styleCard}>
       <img
@@ -33,8 +33,8 @@ const Restaurantcard = () => {
         className="res-logo"
         src="https://static.vecteezy.com/system/resources/thumbnails/025/028/422/small/a-big-breakfast-food-png.png"
       />
-      <h3>Santhosh Dhaba</h3>
-      <h4>Biryani, North Inidan, Asian</h4>
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
       <h4>4.5 stars</h4>
       <h4>38 min</h4>
     </div>
@@ -46,20 +46,11 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
-        <Restaurantcard />
+        <Restaurantcard
+          resName="Santhosh Dhaba"
+          cuisine="Biryani, North Inidan, Asian"
+        />
+        <Restaurantcard resName="Meghna Foods" cuisine="Idly, Dosa" />
       </div>
     </div>
   );
